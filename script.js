08c42536174d78674bfeed1207ad4f109f1c3d23
Anna Konig <annna.konig@gmail.com>
2021-02-04 21:09:26 +0200

    improvement and finishing js

--- script.js
+++ script.js
@@ -2,10 +2,13 @@
 
 
 let funFacts = [
-    { 'fact': "The heads on Easter Island have bodies."},
-    { 'fact': "The moon has moonquaquakes"},
-    { 'fact': "You lose up to 30% of your taste buds during a flight"},
-    { 'fact': "Marie Curie is the only person to earn a Nobel prize in two different sciences"}
+    { 'fact': "The moon has moonquaquakes", 'story': "Blabla" },
+    { 'fact': "You lose up to 30% of your taste buds during a flight", 'story': "Blalfkgmdglkdgkldgmkld"  },
+    { 'fact': "Marie Curie is the only person to earn a Nobel prize in two different sciences", 'story': "asdfghqw12"  },
+    { 'fact': "Space smells like seared steak.", 'story': "1234567" },
+    { 'fact': "The unicorn is the national animal of Scotland." },
+    { 'fact': "Kids ask 300 questions a day." },
+    { 'fact': "Pringles aren't actually potato chips." }
 
 ];
 
@@ -27,8 +30,8 @@ document.body.appendChild(btn);
 //     alert('Click!');
 // }
 
- let facts = document.getElementById("fact");
-
+let facts = document.getElementById("fact");
+let stories = document.getElementById("story");
 
 
 // going to create a function to display the objects in the array randomly
@@ -36,9 +39,10 @@ document.body.appendChild(btn);
 // random generator
 
 document.querySelector("button").onclick = function randomQuotes() {
-    let random = Math.floor(Math.random()*funFacts.length);
+    let random = Math.floor(Math.random() * funFacts.length);
     console.log(random, console.log(funFacts[random].fact));
     facts.innerHTML = funFacts[random].fact;
+    stories.innerHTML = funFacts[random].story;
 
 }
 
