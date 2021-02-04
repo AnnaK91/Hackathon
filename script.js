4317d9dd6c69ecaee82805d8dc24bb298c5c6010
Anna Konig <annna.konig@gmail.com>
2021-02-04 19:14:58 +0200

    creating the array of objects

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-script.js
+// I am creating an array of objects for my fun facts. I am starting with a few to work on the functionality first but I will add more objects later on.
+
+
+let funFacts = [
+    {
+        'fact': "The heads on Easter Island have bodies."
+    },
+    {
+        'fact': "The moon has moonquaquakes"
+    },
+    {
+        'fact': "You lose up to 30% of your taste buds during a flight"
+    },
+    { 'fact': "Marie CUrie is the only person to earn a Nobel prize in two different sciences"}
+        
+];
+
+// console.log(funFacts);
